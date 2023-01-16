cb82145b7f56120551304afe9f0f47a1ac84441d
mkajic20 <mkajic20@foi.hr>
2023-01-16 19:15:24 +0100

    Obrisan nepotreban kod iz klase MainActivity.

--- Software/Hotelzo/app/src/main/java/com/example/hotelzo/MainActivity.kt
+++ Software/Hotelzo/app/src/main/java/com/example/hotelzo/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.hotelzo
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,8 +7,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        //pokretanje stranice registracije za testiranje
-        startActivity(Intent(this,Registration::class.java))
     }
 }
