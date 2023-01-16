3c05cee8da47feb720dcd4b1edae80fc94bafcc0
mkajic20 <mkajic20@foi.hr>
2023-01-16 20:50:49 +0100

    Dodan adapter za fragmente rezervacija. Zadatak #9

--- Software/Hotelzo/app/src/main/java/com/example/hotelzo/AllReservationsActivity.kt
+++ Software/Hotelzo/app/src/main/java/com/example/hotelzo/AllReservationsActivity.kt
@@ -2,8 +2,10 @@ package com.example.hotelzo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
 
 class AllReservationsActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_all_reservations)
