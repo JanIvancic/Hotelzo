e672a51c91f8cbeda4b1e7ac5fc7d235337cbb78
lgaljer20 <lgaljer20@foi.hr>
2023-01-16 12:36:42 +0100

    Izrađena klasa room i izrađen room_list.xml i room_list_item.xml #5

---
+++ Software/Hotelzo/app/src/main/java/com/example/hotelzo/roomViewer/Room.kt
@@ -0,0 +1,9 @@
+package com.example.hotelzo.roomViewer
+
+data class Room(
+    val description: String,
+    val price: Double,
+    val capacity: Short,
+    val image_ulr: String,
+    val label: String
+    )
