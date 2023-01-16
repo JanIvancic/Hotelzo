215f8dea1310285bbc38cf7112f289e3fa969c3e
jivancic20 <jivancic20@foi.hr>
2023-01-16 17:16:49 +0100

    Implementiran obrazac za prijavu sa provjerom korisničkih podataka, uključujući polja za e-mail i lozinku, validaciju i rješavanje pogrešaka, trenutno preusmjeravanje nakon prijave ne funkcionira. Issue #3

---
+++ Software/Hotelzo/app/src/main/java/com/example/hotelzo/LoginActivity.kt
@@ -0,0 +1,89 @@
+package com.example.hotelzo
+
+import android.app.ProgressDialog
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
+import android.widget.Toast
+import androidx.appcompat.app.ActionBar
+import com.example.hotelzo.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var actionBar: ActionBar
+    private lateinit var progressDialog: ProgressDialog
+    private lateinit var firebaseAuth: FirebaseAuth
+    private var email = ""
+    private var password = ""
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        actionBar = supportActionBar!!
+        actionBar.title = "PRIJAVA"
+
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Pričekajte")
+        progressDialog.setMessage("Prijavljivanje...")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        checkUser()
+
+        //REGISTRACIJA
+
+        binding.btnPrijava.setOnClickListener {
+            email = binding.etMail.text.toString().trim()
+            password = binding.etLozinka.text.toString().trim()
+            if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                binding.etMail.error = "@string/test"
+            }
+            else if(TextUtils.isEmpty(password)){
+                binding.etLozinka.error = "@string/test"
+            }
+            else{
+                firebaseLogin()
+            }
+
+            }
+        }
+
+    private fun checkUser() {
+        val firebaseUser = firebaseAuth.currentUser
+        if(firebaseUser != null){
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+    }
+
+    private fun firebaseLogin()  {
+        progressDialog.show()
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+                progressDialog.dismiss()
+                val firebaseUser = firebaseAuth.currentUser
+                val email = firebaseUser!!.email
+                Toast.makeText(this, "Prijavljeni ste kao $email", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }
+            .addOnFailureListener {
+                progressDialog.dismiss()
+                Toast.makeText(this, "Neuspješna prijava", Toast.LENGTH_SHORT).show()
+            }
+    }
+}
+    /*
+    private fun checkUser() {
+        val firebaseUser = firebaseAuth.currentUser
+        if (firebaseUser != null) {
+            startActivity(Intent(this, ProfileActivity::class.java))
+            finish()
+        }
+    }
+    */
--- Software/Hotelzo/app/src/main/java/com/example/hotelzo/MainActivity.kt
+++ Software/Hotelzo/app/src/main/java/com/example/hotelzo/MainActivity.kt
@@ -2,13 +2,25 @@ package com.example.hotelzo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.util.Log
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val firestore = Firebase.firestore
+        val korisnikRef = firestore.collection("Korisnik")
+        val korisnikDocs = korisnikRef.get()
+        korisnikDocs.addOnSuccessListener { result ->
+            for (document in result) {
+                Log.d("Korisnik", "${document.id} => ${document.data.get("id_uloge")}")
+            }
+        }
 
     }
 }
