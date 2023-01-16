8007ae6132e39e0f47fe6c3cc010a3477df9d73a
lgaljer20 <lgaljer20@foi.hr>
2023-01-16 22:05:32 +0100

    Dodan RecyclerViewRoom.kt i RoomAdapter.kt za prikaz liste sobe  #5

--- Software/Hotelzo/app/src/main/java/com/example/hotelzo/roomViewer/RecyclerViewRoom.kt
+++ Software/Hotelzo/app/src/main/java/com/example/hotelzo/roomViewer/RecyclerViewRoom.kt
@@ -1,19 +1,45 @@
 package com.example.hotelzo.roomViewer
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.hotelzo.R
-import com.google.firebase.ktx.Firebase
+import com.google.firebase.firestore.FirebaseFirestore
+
 
 class RecyclerViewRoom : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var roomList: ArrayList<Room>
-  
+    private lateinit var db: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.room_list)
+
+        recyclerView = findViewById(R.id.recycler_view_rooms)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        roomList = arrayListOf()
+        db = FirebaseFirestore.getInstance()
+        db.collection("Soba").get().addOnSuccessListener {
+            if(!it.isEmpty){
+                for (data in it.documents){
+                    val room: Room? = data.toObject(Room::class.java)
+                    if (room != null) {
+                        roomList.add(room)
+
+                    }
+                }
+                Log.d("RecyclerViewRoom", "Data loaded successfully")
+                recyclerView.adapter = RoomAdapter(roomList)
+            }
+        }
+            .addOnFailureListener {
+                Toast.makeText( this, it.toString(), Toast.LENGTH_SHORT).show()
+            }
     }
 }
--- Software/Hotelzo/app/src/main/java/com/example/hotelzo/roomViewer/RoomAdapter.kt
+++ Software/Hotelzo/app/src/main/java/com/example/hotelzo/roomViewer/RoomAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.hotelzo.roomViewer
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,12 +8,13 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.hotelzo.R
+import com.bumptech.glide.Glide
 
 class RoomAdapter(private val roomList:ArrayList<Room>) : RecyclerView.Adapter<RoomAdapter.MyViewHolder>() {
 
     class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        val price : TextView = itemView.findViewById(R.id.room_price)
-        val description : TextView = itemView.findViewById(R.id.room_description)
+        val cijena_sobe : TextView = itemView.findViewById(R.id.room_price)
+        val opis_sobe : TextView = itemView.findViewById(R.id.room_description)
         val image : ImageView = itemView.findViewById(R.id.room_image)
         val capacity: TextView = itemView.findViewById(R.id.room_capacity)
     }
@@ -20,12 +22,16 @@ class RoomAdapter(private val roomList:ArrayList<Room>) : RecyclerView.Adapter<R
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val itemView =
             LayoutInflater.from(parent.context).inflate(R.layout.room_list_item, parent, false)
+        Log.d("RoomAdapter", "View inflated successfully")
         return MyViewHolder(itemView)
     }
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.capacity.text = roomList[position].capacity.toString()
-        holder.description.text = roomList[position].description
-        holder.price.text = roomList[position].price.toString()
+        holder.opis_sobe.text = roomList[position].kratak_opis
+        holder.capacity.text = "Kapacitet: "+roomList[position].kapacitet.toString()
+        holder.cijena_sobe.text = roomList[position].cijena_sobe.toString() + "€"
+        Glide.with(holder.itemView.context)
+            .load(roomList[position].slika_url)
+            .into(holder.image)
 
     }
 
