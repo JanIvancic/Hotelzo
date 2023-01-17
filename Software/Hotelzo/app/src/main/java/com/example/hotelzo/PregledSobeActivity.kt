b66562e6b38de117402db3e5db23785d674ecd6b
jivancic20 <jivancic20@foi.hr>
2023-01-17 11:00:49 +0100

    Prikazivanje informacija o sobi, inforamcije se prosljeđuju pomoću konstruktora funkciji Issue #6

---
+++ Software/Hotelzo/app/src/main/java/com/example/hotelzo/PregledSobeActivity.kt
@@ -0,0 +1,40 @@
+package com.example.hotelzo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class PregledSobeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pregled_sobe)
+
+        val cijenaSobe = intent.getDoubleExtra("cijenaSobe", 0.0)
+        val kapacitet = intent.getIntExtra("kapacitet", 0)
+        val opisSobe = intent.getStringExtra("opisSobe")
+        val oznakaSobe = intent.getStringExtra("oznakaSobe")
+
+        val tvOznakaSobe = findViewById<TextView>(R.id.tv_oznaka_sobe)
+        tvOznakaSobe.text = "Oznaka sobe: $oznakaSobe"
+
+        val tvOpisSobe = findViewById<TextView>(R.id.tv_opis_sobe)
+        tvOpisSobe.text = "Opis sobe: $opisSobe"
+
+        val tvInternet = findViewById<TextView>(R.id.tv_internet)
+        if(true){
+            tvInternet.text = "Internet: Da"
+        }else{
+            tvInternet.text = "Internet: Ne"
+        }
+
+        val tvCijenaSobe = findViewById<TextView>(R.id.tv_cijena_sobe_value)
+        tvCijenaSobe.text = cijenaSobe.toString()
+
+        val tvKapacitet = findViewById<TextView>(R.id.tv_kapacitet_value)
+        tvKapacitet.text = kapacitet.toString()
+    }
+
+}
+
+
