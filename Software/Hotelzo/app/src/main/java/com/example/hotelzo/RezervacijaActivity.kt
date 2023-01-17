0b96cda275f010c712d4e8c1ed782460625ea180
jivancic20 <jivancic20@foi.hr>
2023-01-17 11:29:08 +0100

    Napravljen dizajn za rezervaciju sobe u određenom terminu Issue #7

--- Software/Hotelzo/app/src/main/java/com/example/hotelzo/RezervacijaActivity.kt
+++ Software/Hotelzo/app/src/main/java/com/example/hotelzo/RezervacijaActivity.kt
@@ -1,15 +1,11 @@
 package com.example.hotelzo
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class RezervacijaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        startActivity(Intent(this,RezervacijaActivity::class.java))
+        setContentView(R.layout.activity_rezervacija)
     }
-
-
 }
