1a7b26daba1aa71d9d5af01e136bb12a54924944
jivancic20 <jivancic20@foi.hr>
2023-01-17 11:31:39 +0100

    Klikom na odabrani datum, on se prikazuje u polju ispod, treba napraviti da se dolazak i odlazak razlikuju Issue #7

--- Software/Hotelzo/app/src/main/java/com/example/hotelzo/RezervacijaActivity.kt
+++ Software/Hotelzo/app/src/main/java/com/example/hotelzo/RezervacijaActivity.kt
@@ -2,10 +2,25 @@ package com.example.hotelzo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CalendarView
+import android.widget.TextView
 
 class RezervacijaActivity : AppCompatActivity() {
+    private lateinit var datumDolaska: TextView
+    private lateinit var datumOdlaska: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rezervacija)
+
+        datumDolaska = findViewById(R.id.dolazak)
+        datumOdlaska = findViewById(R.id.odlazak)
+
+        val calendarView = findViewById<CalendarView>(R.id.calendar_view)
+        calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            val date = "$dayOfMonth-${month + 1}-$year"
+            datumDolaska.text = "Datum dolaska: $date"
+            datumOdlaska.text = "Datum odlaska: $date"
+        }
     }
 }
