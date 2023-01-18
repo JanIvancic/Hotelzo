85e627b9b296c61b6b131cb67b0a05e536e72097
lgaljer20 <lgaljer20@foi.hr>
2023-01-18 12:47:48 +0100

    Dodana logika za funckionalnosti filtera, rade dobro, ne rade perfektno. Lista soba se može filtrirati.  #5

--- Software/Hotelzo/app/src/main/java/com/example/hotelzo/roomViewer/RecyclerViewRoom.kt
+++ Software/Hotelzo/app/src/main/java/com/example/hotelzo/roomViewer/RecyclerViewRoom.kt
@@ -3,10 +3,8 @@ package com.example.hotelzo.roomViewer
 
 import android.os.Bundle
 import android.util.Log
-import android.widget.ArrayAdapter
-import android.widget.ImageView
-import android.widget.Spinner
-import android.widget.Toast
+import android.view.View
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -18,11 +16,15 @@ class RecyclerViewRoom : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var roomList: ArrayList<Room>
     private lateinit var db: FirebaseFirestore
+    private var filteredRoomList = arrayListOf<Room>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.room_list)
         val btnBack = findViewById<ImageView>(R.id.back_arrow)
+        val btnResetFilters = findViewById<ImageView>(R.id.imageView_reset_filter)
+
+
 
         btnBack.setOnClickListener{
             finish()
@@ -45,6 +47,11 @@ class RecyclerViewRoom : AppCompatActivity() {
                 recyclerView.adapter = RoomAdapter(roomList)
             }
 
+            val priceFilterSpinner = findViewById<Spinner>(R.id.price_filter_spinner)
+            var filteredByCapacity = roomList
+            var filteredByPrice = roomList
+            var selectedCapacity = ""
+            var selectedPrice= ""
             val options = arrayOf("Sve", "1", "2", "3")
             val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, options)
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
@@ -53,13 +60,72 @@ class RecyclerViewRoom : AppCompatActivity() {
 
             spinner.setSelection(0);
 
+            spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                }
+
+
+                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                    selectedCapacity = options[position]
+
+                    if (selectedCapacity != "Sve") {
+
+                        filteredByCapacity= roomList.filter { it.kapacitet == selectedCapacity.toInt() } as ArrayList<Room>
+                    } else {
+                        filteredByCapacity = roomList
+                    }
+                    filteredRoomList = filteredByCapacity
+                    recyclerView.adapter = RoomAdapter(filteredRoomList)
+
+                }
+            }
+
 
             val priceRanges = arrayOf("Sve", "50€ - 75€", "75€ - 100€", "100€ - 125€", "125€ - 150€")
+            val priceRangesMap = mapOf("50€ - 75€" to 50..75, "75€ - 100€" to 75..100, "100€ - 125€" to 100..125, "125€ - 150€" to 125..150)
             val adapterPrice = ArrayAdapter(this, android.R.layout.simple_spinner_item, priceRanges)
             adapterPrice.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            val priceFilterSpinner = findViewById<Spinner>(R.id.price_filter_spinner)
+
             priceFilterSpinner.adapter = adapterPrice
             priceFilterSpinner.setSelection(0);
+
+            priceFilterSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+
+                override fun onNothingSelected (parent: AdapterView<*>?) {
+
+                }
+
+
+                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                    fun filterByPrice(roomList: ArrayList<Room>, priceRange: IntRange): ArrayList<Room> {
+                        return ArrayList(roomList.filter { it.cijena_sobe in priceRange })
+                    }
+
+                    selectedPrice = priceRanges[position]
+
+
+                    if (selectedPrice != "Sve") {
+                        if(selectedCapacity != "Sve"){
+                            filteredByPrice= filterByPrice(filteredByCapacity, priceRangesMap[selectedPrice]!!)
+                            filteredRoomList = filteredByPrice
+                        }else{
+                            filteredRoomList= filterByPrice(roomList, priceRangesMap[selectedPrice]!!)
+                        }
+
+                    } else {
+                        filteredRoomList = roomList
+                    }
+                    recyclerView.adapter = RoomAdapter(filteredRoomList)
+                }
+            }
+
+            btnResetFilters.setOnClickListener{
+                spinner.setSelection(0)
+                selectedCapacity = ""
+                priceFilterSpinner.setSelection(0)
+                filteredRoomList = roomList
+                recyclerView.adapter = RoomAdapter(filteredRoomList)
+            }
         }
             .addOnFailureListener {
                 Toast.makeText( this, it.toString(), Toast.LENGTH_SHORT).show()
