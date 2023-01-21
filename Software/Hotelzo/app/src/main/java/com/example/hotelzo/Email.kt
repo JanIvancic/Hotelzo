d7d6928dba7e245748bd707d37786c3ff889c0db
mkajic20 <mkajic20@foi.hr>
2023-01-21 23:50:21 +0100

    Implementirano slanje maila o uspješnoj rezervaciji sobe. Zadatak #8

--- Software/Hotelzo/app/src/main/java/com/example/hotelzo/Email.kt
+++ Software/Hotelzo/app/src/main/java/com/example/hotelzo/Email.kt
@@ -1,5 +1,7 @@
 package com.example.hotelzo
 
+
+import android.content.Context
 import android.util.Log
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -11,7 +13,21 @@ import okhttp3.RequestBody.Companion.toRequestBody
 
 class Email {
     companion object{
-        private const val apiKey = "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"
+        private val apiKey = "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"
+
+        fun sendReservationEmail(context: Context, email:String, name:String, dateStart: String, dateEnd: String, room:String, price:String){
+            val message = context.getString(R.string.email_greeting) + " " +  name +  "<br>" +
+                    context.getString(R.string.email_room_reserved) + " " +  room + "<br>" +
+                    context.getString(R.string.email_reservation_start) + " " + dateStart + "<br>" +
+                    context.getString(R.string.email_reservation_end) + " " + dateEnd + "<br>" +
+                    context.getString(R.string.email_price) + " " + price + "<br>" +
+                    context.getString(R.string.email_farewell) + "<br>" +
+                    context.getString(R.string.app_name)
+
+            val subject = context.getString(R.string.email_subject_reservation)
+
+            this.sendEmail(message, subject, email, name)
+        }
 
         fun sendEmail(message: String, subject:String, email: String, name: String){
             val json = """
@@ -47,7 +63,6 @@ class Email {
                     Log.d("Email error", e.toString())
                 }
             }
-
 
         }
     }
