9eeff10069adcccc8053a528043baba73209c0fc
mkajic20 <mkajic20@foi.hr>
2023-01-22 00:01:27 +0100

    Implementirana funkcija za slanje maila o otkazanoj rezervaciji Zadatak #8

--- Software/Hotelzo/app/src/main/java/com/example/hotelzo/Email.kt
+++ Software/Hotelzo/app/src/main/java/com/example/hotelzo/Email.kt
@@ -15,6 +15,19 @@ class Email {
     companion object{
         private val apiKey = "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"
 
+        fun sendCancelationEmail(context: Context, email: String, name: String,  dateStart: String, dateEnd: String, room: String){
+            val message = context.getString(R.string.email_greeting) + " " +  name +  "<br>" +
+                    context.getString(R.string.your_reservation) + " " + room + " " +
+                    context.getString(R.string.from) + " " + dateStart + " " +
+                    context.getString(R.string.to) + " " + dateEnd + " " + context.getString(R.string.cancelled) + "<br>" +
+                    context.getString(R.string.email_farewell) + "<br>" +
+                    context.getString(R.string.app_name)
+
+            val subject = context.getString(R.string.email_subject_cancelled)
+
+            this.sendEmail(message, subject, email, name)
+        }
+
         fun sendReservationEmail(context: Context, email:String, name:String, dateStart: String, dateEnd: String, room:String, price:String){
             val message = context.getString(R.string.email_greeting) + " " +  name +  "<br>" +
                     context.getString(R.string.email_room_reserved) + " " +  room + "<br>" +
