0fd005d8eeb3529c84aa9003c0a69e38d092237a
jivancic20 <jivancic20@foi.hr>
2023-01-26 10:18:20 +0100

    Datum se sprema u formatu timestemp u bazu. Issue #7

--- Software/Hotelzo/app/src/main/java/com/example/hotelzo/RezervacijaActivity.kt
+++ Software/Hotelzo/app/src/main/java/com/example/hotelzo/RezervacijaActivity.kt
@@ -6,19 +6,24 @@ import android.widget.CalendarView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.FirebaseFirestore
+import java.lang.String.format
 import java.text.SimpleDateFormat
+import com.google.firebase.Timestamp
 import java.util.*
 
 class RezervacijaActivity : AppCompatActivity() {
-
+    private lateinit var db: FirebaseFirestore
     private lateinit var checkInDateTextView: TextView
     private lateinit var checkOutDateTextView: TextView
-    private var checkInDate: Long = 0
-    private var checkOutDate: Long = 0
+    private var checkInTimestamp: Timestamp? = null
+    private var checkOutTimestamp: Timestamp? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rezervacija)
+        db = FirebaseFirestore.getInstance()
 
         checkInDateTextView = findViewById(R.id.tv_check_in_date)
         checkOutDateTextView = findViewById(R.id.tv_check_out_date)
@@ -26,46 +31,68 @@ class RezervacijaActivity : AppCompatActivity() {
         val removeDrugi = findViewById<TextView>(R.id.removeDrugi)
         val calendarView = findViewById<CalendarView>(R.id.calendar_view)
 
-        setUpReservationButton();
+        setUpReservationButton()
 
         calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
             val selectedDate = Calendar.getInstance()
             selectedDate.set(year, month, dayOfMonth)
-            //setUpReservationButton();
-
-            if (checkInDate == 0L) {
-                checkInDate = selectedDate.timeInMillis
-                checkInDateTextView.text = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).format(selectedDate.time)
+            val selectedTimestamp = Timestamp(selectedDate.time)
+            if (checkInTimestamp == null) {
+                checkInTimestamp = selectedTimestamp
+                checkInDateTextView.text = SimpleDateFormat("MM/dd/yyyy").format(selectedTimestamp.toDate())
             } else {
-                checkOutDate = selectedDate.timeInMillis
-                checkOutDateTextView.text = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).format(selectedDate.time)
+                checkOutTimestamp = selectedTimestamp
+                checkOutDateTextView
+                    .text = SimpleDateFormat("MM/dd/yyyy").format(selectedTimestamp.toDate())
             }
         }
 
         removePrvi.setOnClickListener {
-            checkInDate = 0
-            checkInDateTextView.setText("")
+            checkInTimestamp = null
+            checkInDateTextView.text = ""
         }
         removeDrugi.setOnClickListener {
-            checkOutDate = 0
-            checkOutDateTextView.setText("")
+            checkOutTimestamp = null
+            checkOutDateTextView.text = ""
         }
     }
+
     private fun setUpReservationButton() {
         val btn_rezerviraj = findViewById<Button>(R.id.btn_rezerviraj)
         btn_rezerviraj.setOnClickListener {
-            if (checkInDate != 0L && checkOutDate != 0L) {
-                val checkIn = Calendar.getInstance().apply { timeInMillis = checkInDate }
-                val checkOut = Calendar.getInstance().apply { timeInMillis = checkOutDate }
-                if (checkOut.before(checkIn)) {
-                    Toast.makeText(this, "Datum odlaska ne može biti prije datuma dolaska.", Toast.LENGTH_SHORT).show()
-                    return@setOnClickListener
+            if (checkInTimestamp == null || checkOutTimestamp == null) {
+                if (checkInTimestamp == null && checkOutTimestamp == null) {
+                    Toast.makeText(
+                        this, "Molim vas izaberite datum dolaska i odlaska.", Toast.LENGTH_SHORT
+                    ).show()
+                } else if (checkInTimestamp == null) {
+                    Toast.makeText(
+                        this, "Molim vas izaberite datum dolaska.", Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    Toast.makeText(
+                        this, "Molim vas izaberite datum odlaska.", Toast.LENGTH_SHORT
+                    ).show()
                 }
-                // Perform the reservation action
             } else {
-                // Display message to user that check-in and check-out dates are required
-                Toast.makeText(this, "Odaberite datum dolaska i odlaska.", Toast.LENGTH_SHORT).show()
+                addRezarvaciju(checkInTimestamp!!, checkOutTimestamp!!)
             }
         }
+    }
+
+    private fun addRezarvaciju(checkInTimestamp: Timestamp, checkOutTimestamp: Timestamp) {
+        val dbReference = db.collection("Rezervacija")
+        val newRezervacija = hashMapOf(
+            "datum_kraj" to checkOutTimestamp,
+            "datum_pocetak" to checkInTimestamp
+        )
+        dbReference.add(newRezervacija)
+            .addOnSuccessListener {
+                Toast.makeText(this, "Rezervacija uspješno kreirana", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+            .addOnFailureListener { e ->
+                Toast.makeText(this, "Greška: $e", Toast.LENGTH_SHORT).show()
+            }
     }
 }
