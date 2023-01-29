a54b83c460b9894f57d55d8988c58ccf185a632c
lgaljer20 <lgaljer20@foi.hr>
2023-01-29 12:42:26 +0100

    Dodana logika da se rezervacije izbriše pritiskom na gumb za brisanje te da se trenutna lista osvježi #10

--- Software/Hotelzo/app/src/main/java/com/example/hotelzo/adapters/ReservationsAdapter.kt
+++ Software/Hotelzo/app/src/main/java/com/example/hotelzo/adapters/ReservationsAdapter.kt
@@ -1,19 +1,27 @@
 package com.example.hotelzo.adapters
 
+
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.hotelzo.AllReservationsActivity
 import com.example.hotelzo.R
 import com.example.hotelzo.data.Reservations
 
-class ReservationsAdapter(private val reservationList: ArrayList<Reservations>) : RecyclerView.Adapter<ReservationsAdapter.reservationViewHolder>() {
+import kotlin.collections.ArrayList
+
+
+class ReservationsAdapter(private val reservationList: ArrayList<Reservations>,  private val activity: AllReservationsActivity) : RecyclerView.Adapter<ReservationsAdapter.reservationViewHolder>() {
     class reservationViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val tvName: TextView = itemView.findViewById(R.id.txt_name)
         val tvRoomLabel: TextView = itemView.findViewById(R.id.txt_room)
         val tvStartDate: TextView = itemView.findViewById(R.id.date_start)
         val tvEndDate: TextView = itemView.findViewById(R.id.date_end)
+        val ivDeleteButton: ImageView= itemView.findViewById(R.id.btn_delete_reservation)
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): reservationViewHolder {
@@ -26,7 +34,14 @@ class ReservationsAdapter(private val reservationList: ArrayList<Reservations>) 
         holder.tvRoomLabel.text = reservationList[position].room_label
         holder.tvStartDate.text = reservationList[position].start_date.toString()
         holder.tvEndDate.text = reservationList[position].end_date.toString()
+        holder.ivDeleteButton.setOnClickListener{
+            activity.deleteReservation(reservationList[position].document_id)
+            reservationList.removeAt(position)
+            notifyDataSetChanged()
+        }
     }
+
+
 
     override fun getItemCount(): Int {
         return reservationList.size
